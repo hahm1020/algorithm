7f7dbb5fafbfcb7baffc3481f61b643196177f0f
hahm <hahm1020@naver.com>
2023-04-09 16:27:41 +0900

    입력받은 문자열의 길이

---
+++ fun/src/characterString/WordLength.java
@@ -0,0 +1,10 @@
+package characterString;
+
+import java.util.Scanner;
+
+public class WordLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().length());
+    }
+}
